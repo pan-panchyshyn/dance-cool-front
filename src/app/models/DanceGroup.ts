ecaf82a821a831750e1a83992ca39893cdaf962b
BestCase13 <panchyshyn.andrew@gmail.com>
2019-09-24 12:30:54 +0300

    Rebuilded model structure

--- src/app/models/DanceGroup.ts
+++ src/app/models/DanceGroup.ts
@@ -1,11 +1,9 @@
+import { User } from './User';
+
 export class DanceGroup {
   groupId: number;
   groupDirection: string;
-  primaryMentorId: number;
-  primaryMentorFirstName: string;
-  primaryMentorLastName: string;
-  secondaryMentorId: number;
-  secondaryMentorFirstName: string;
-  secondaryMentorLastName: string;
+  primaryMentor: User;
+  secondaryMentor?: User;
   groupLevel: string;
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,7 +1,9 @@
+import { Role } from './Role';
+
 export class User {
   id: number;
   firstName: string;
   lastName: string;
   phoneNumber: string;
-  roleId?: number;
+  role?: Role;
 }
